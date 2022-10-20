2f15f152ebf2888e9d3c366754848d2ec1d09f44
Jiyeon <ziyeon.ivi@gmail.com>
2022-10-21 02:21:01 +0900

    Test Habit component

--- src/components/habit/habit.jsx
+++ src/components/habit/habit.jsx
@@ -17,13 +17,25 @@ const Habit = memo(({ habit, onIncrement, onDecrement, onDelete }) => {
     <li className="habit">
       <span className="habit-name">{habit.name}</span>
       <span className="habit-count">{habit.count}</span>
-      <button className="habit-button habit-increase" onClick={handleIncrement}>
+      <button
+        className="habit-button habit-increase"
+        title="increase"
+        onClick={handleIncrement}
+      >
         <i className="fas fa-plus-square"></i>
       </button>
-      <button className="habit-button habit-decrease" onClick={handleDecrement}>
+      <button
+        className="habit-button habit-decrease"
+        title="decrease"
+        onClick={handleDecrement}
+      >
         <i className="fas fa-minus-square"></i>
       </button>
-      <button className="habit-button habit-delete" onClick={handleDelete}>
+      <button
+        className="habit-button habit-delete"
+        title="delete"
+        onClick={handleDelete}
+      >
         <i className="fas fa-trash"></i>
       </button>
     </li>
